a3bc5810cdf21043f2844f7744926842b0084b84
SaitejaVarada1122 <teja.varada.1122@gmail.com>
2025-12-18 19:30:47 +0530

    code

---
+++ src/main/java/com/learn/java_githubactions/JavaGithubactionsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.java_githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGithubactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGithubactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/java_githubactions/JavaGithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.java_githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGithubactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
